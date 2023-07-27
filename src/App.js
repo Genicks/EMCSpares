6e44df38d7be8b2978fb081a9407a4c388da97ce
Genick's <Genicks.html@gmail.com>
2023-07-27 15:35:51 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./CSS/App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import { BrowserRouter } from "react-router-dom";
+
 
 function App() {
   return (
-    <div>
+    <BrowserRouter basename="EMCSpares">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
       </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -4,26 +4,49 @@ import LogoFooter from "../Img/EMCLogo.png";
 const Footer = () => {
   return (
     <div className="Footer">
-
       <div className="ConLogoFooter">
-        <img src={LogoFooter} alt="EMC SPARES" className="LogoFooter"/>
+        <img src={LogoFooter} alt="EMC SPARES" className="LogoFooter" />
         <a href="/" className="LogoLabel">
           EMC SPARES
         </a>
       </div>
 
-      <div className="ConLinks ">
+      <div className="ConLinks">
         <a
-          className="Link "
+          className="Link Link1"
+          href="https://call.whatsapp.com/voice/FagIjZ9kgmBFmNkJnWcEZE"
+        >
+          <i className="bi bi-telephone me-2 Link1"></i>Phone
+        </a>
+
+        <a
+          className="Link Link2"
+          href="mailto:genicks.html@gmail.com?subject=EMCSpares%20Customer&body=I%20would%20like%20to%20have%20some%20assistance."
+        >
+          <i className="bi bi-envelope me-2 Link2"></i>
+          Email
+        </a>
+
+        <a
+          className="Link Link3"
+          href="https://api.whatsapp.com/send?phone=5926578699&text=I%20would%20like%20to%20make%20an%20query"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="bi bi-whatsapp me-2 Link3"></i>
+          Whatsapp
+        </a>
+
+        <a
+          className="Link Link4"
           href="http://"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i class="bi bi-whatsapp me-1"></i>
-          Whatsapp
+          <i className="bi bi-facebook me-2 Link4"></i>
+          Facebook
         </a>
       </div>
-
     </div>
   );
 };
