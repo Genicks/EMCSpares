c4afa90f52486bee0f27603ba68b61d94a1b103c
Genick's <Genicks.html@gmail.com>
2023-08-09 17:47:22 -0400

    update about page

--- src/Component/Cover/Cover.jsx
+++ src/Component/Cover/Cover.jsx
@@ -5,15 +5,18 @@ const Cover = () => {
     <div className="Cover container-fluid">
       <div>
         <div className="Container">
-
           <h1 className="d-flex justify-content-center">
             <div className="Info Heading">Spare parts for sale!</div>
           </h1>
 
           <h1 className="d-flex justify-content-center">
-            <a href="https://api.whatsapp.com/send?phone=5926578699&text=I%20would%20like%20to%20make%20an%20order." className="orderWord">Order Now!</a>
+            <a
+              href="https://api.whatsapp.com/send?phone=5926578699&text=I%20would%20like%20to%20make%20an%20order."
+              className="orderWord"
+            >
+              Order Now!
+            </a>
           </h1>
-
         </div>
 
         <div className="d-flex justify-content-center">
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,43 @@
 import NavBar from "../Component/NavBar/NavBar";
+import Footer from "../Component/Footer/Footer";
+import "../CSS/About.css";
 
 const About = () => {
   return (
     <div>
       <NavBar />
-      <h1>This is the about Page</h1>
+      <div className="About">
+        <div className="BackSplash"></div>
+        <h1>EMC SPARES</h1>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error
+            aliquid consectetur temporibus, corrupti veritatis recusandae quas
+            illum ea soluta voluptas quia! Nisi iusto velit voluptatum error
+            magni suscipit cum commodi. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Corporis perferendis quae recusandae, minus fugit
+            consectetur. Neque, distinctio dolore esse non fuga officiis ipsam
+            aut laudantium ut in, itaque hic eligendi.
+          </p>
+          <code>        
+            Built and Maintain by: id="NQF Web-Developement"
+          </code>
+        </div>
+        <br />
+        <div className="content">
+          <h4>How to make an order</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error
+            aliquid consectetur temporibus, corrupti veritatis recusandae quas
+            illum ea soluta voluptas quia! Nisi iusto velit voluptatum error
+            magni suscipit cum commodi. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Corporis perferendis quae recusandae, minus fugit
+            consectetur. Neque, distinctio dolore esse non fuga officiis ipsam
+            aut laudantium ut in, itaque hic eligendi.
+          </p>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
